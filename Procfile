b2a982976aa34d2419d184fd9e32e4c2c44b3947
chillzlamez <deveshreddy27@gmail.com>
2018-10-25 16:07:54 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node root.js
+ 
--- root.js
+++ root.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ ironman: "first print" });
+  res.send({ ironman: "first printinhg" });
 });
-
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
