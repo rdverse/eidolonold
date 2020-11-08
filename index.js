84a3181fe93c3436c3f1c3e3a9a57a26d663a459
redevverse <deveshreddy27@gmail.com>
2020-11-07 21:02:17 -0600

    google oauth flow

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 // Using common js modules imports here
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const  keys = require('./config/keys');
 
+
+//App declaration
 const app = express();
 
-// define route handler
-app.get('/', (req, res) => {
-    res.send({Hi : 'TiiiiiiingYuuuuuuuuun'});
-});
+
+passport.use(
+    new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: "/auth/google/callback"
+    },
+
+		       (accessToken, refreshToken, profile, done) => {
+			   console.log('accessToken', accessToken);
+			   console.log('refreshToken', refreshToken);
+			   console.log('profile', profile);
+			    console.log('done', done);
+
+		       }));
+
+//Add a route handler for the passport block
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']})
+       );
+
+//second route handler for route handler
+// (similar) but passport now tries to kick back the user to the callback url
+// At the callback url,we get an access token which we will use to verify the user
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 // Get the environment variable port from heroku
 // dev - 5000 , prod - PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT);
+
